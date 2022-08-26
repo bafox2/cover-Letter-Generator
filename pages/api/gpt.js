eb7e777d1d1b7ad833c3563705888023379d4aa8
ben <benjaminalexanderfox11@gmail.com>
2022-08-26 12:12:29 -0400

    got some sort of model that seems good

--- pages/api/gpt.js
+++ pages/api/gpt.js
@@ -4,32 +4,19 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(configuration)
+export default async function (req, res) {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: generatePrompt(req.body),
+    temperature: 0.54,
+    max_tokens: 3792,
+    top_p: 0.56,
+    frequency_penalty: 1.54,
+    presence_penalty: 1.52,
+  })
+  res.status(200).json({ result: response.data.choices[0].text })
+}
 
-const response = await openai.createEdit({
-  model: 'text-davinci-002',
-  input:
-    'write a cover letter to a company highlighting your strengths and what you know about the company.\nStrengths: hard worker, familiar with the stack, \
-    local\nCompany: great cause, solid leadership, mentoring program',
-  instruction: '',
-  temperature: 0.5,
-  top_p: 1,
-})
-
-//this is how you can protect an api route, will need to integrate the two of these things
-import { unstable_getServerSession } from 'next-auth/next'
-import { authOptions } from './auth/[...nextauth]'
-
-export default async (req, res) => {
-  const session = await unstable_getServerSession(req, res, authOptions)
-
-  if (session) {
-    res.send({
-      content:
-        'This is protected content. You can access this content because you are signed in.',
-    })
-  } else {
-    res.send({
-      error: 'You must be sign in to view the protected content on this page.',
-    })
-  }
+function generatePrompt(data) {
+  return `Create 200 word cover letter with the following parameters:\nCompany: ${data.company}\nCompany highlights: ${data.highlights}\nPosition: ${data.position}\nJob Listing: ${data.jobListing}`
 }
