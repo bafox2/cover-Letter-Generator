c6aa2e13615d4978fd23b4ed4ca81f2c4561981f
ben <benjaminalexanderfox11@gmail.com>
2022-08-26 11:09:13 -0400

    form created and routed

--- pages/[name]/requests/index.js
+++ pages/[name]/requests/index.js
@@ -1,19 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../../styles/Home.module.scss'
-import { useForm } from 'react-hook-form'
 
 export default function QueryPage() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm()
-  const onSubmit = (data) => {
-    console.log(data)
-  }
-
   return (
     <div className={styles.container}>
       <Head>
@@ -24,28 +13,6 @@ export default function QueryPage() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Your Requests</h1>
-
-        <div className={styles.grid}>
-          <a className={styles.card}>
-            <h2>Your Strengths</h2>
-            <input
-              type="text"
-              className="input"
-              {...register('strengths', { required: true })}
-            />
-          </a>
-
-          <a className={styles.card}>
-            <h2>Company Highlights</h2>
-            <input
-              type="text"
-              className="input"
-              {...register('highlights', { required: true })}
-            />
-          </a>
-        </div>
-
-        <button type="submit">Submit</button>
       </main>
     </div>
   )
