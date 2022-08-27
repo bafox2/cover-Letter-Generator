4f629bee16be5c8ae87c41ca864872566afa564f
ben <benjaminalexanderfox11@gmail.com>
2022-08-26 20:13:56 -0400

    page added for unauthenticated users

---
+++ pages/unauthenticated.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Unauthenticated = () => {
+  return (
+    <div>
+      <h1>Unauthenticated</h1>
+      <h2>Please log into your github account to view this.</h2>
+    </div>
+  )
+}
+
+export default Unauthenticated
