4a939323da77596fdd2c8c9835274879ec0ef099
ben <benjaminalexanderfox11@gmail.com>
2022-09-27 18:31:03 -0400

    added correct information

--- pages/about.js
+++ pages/about.js
@@ -4,16 +4,16 @@ import styles from '../styles/Home.module.scss'
 export default function About() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>About</h1>
+      <h1 className={styles.title}>Tools</h1>
 
       <p className={styles.description}>
-        A few of the tools used to make this tool.
+        A few of the technologies used to make this app.
       </p>
 
       <div className={styles.grid}>
         <a href="https://nextjs.org/docs" className={styles.card}>
           <h2>Next js</h2>
-          <p>
+          <p className={styles.cardText}>
             Next js gave is a &lsquono assembly required &lsquo tool to give a
             great React developer experience.
           </p>
@@ -21,7 +21,7 @@ export default function About() {
 
         <a href="https://next-auth.js.org/docs" className={styles.card}>
           <h2>Next-Auth</h2>
-          <p>
+          <p className={styles.cardText}>
             Next-Auth is a great authentication system for Next.js. It&#39;s
             built on top of Passport.js.
           </p>
@@ -29,7 +29,7 @@ export default function About() {
 
         <a href="https://openai.com" className={styles.card}>
           <h2>Openai</h2>
-          <p>
+          <p className={styles.cardText}>
             Openai is a great tool to use for machine learning, and a fountain
             of inspiration for possibilities in the future.
           </p>
@@ -37,7 +37,7 @@ export default function About() {
 
         <a href="https://mongoosejs.com" className={styles.card}>
           <h2>Mongoose</h2>
-          <p>
+          <p className={styles.cardText}>
             Mongoose is the database for this project, and is a great tool to
             use for creating and updating models.
           </p>
